1ccfe828451ba2f3fbbcd7331ae1474ef5120805
m-dopazo <mdopazo20@gmail.com>
2023-10-02 22:05:18 -0300

    Lo que va
    asd

--- index.js
+++ index.js
@@ -2,36 +2,46 @@ const url = `https://japceibal.github.io/emercado-api/cats_products/101.json`;
 
 document.addEventListener('DOMContentLoaded', function(){
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => crearFichas(data))
+  //Fetch Lista productos
+  fetch(url)
+  .then(response => response.json())
+  .then(data => crearFichas(data))
+  .catch(error => alert(error));
 
-
+  //Añadir nombre
+  nombre = prompt("Inserte tu nombre:");
+  insertarNombre(nombre);
+  
 })
 
-fetch(url)
+/* fetch(url)
 .then(response => response.json())
-.then(data => crearFichas(data))
+.then(data => crearFichas(data)) */
   
 function crearFichas(registro){
    
-    const producto = document.getElementById(producto)
+  const producto = document.getElementById("listaProductos");
   
-    for (const i of registro.products){
-      producto.innerHTML +=
-        `
-        <div class="articulo" onclick ="viajar(${i.id})">
-          <div class="imagenDeArticulo">
-              <img src= ${i.image}></img>
-          </div>
-          <div class="descDeArticulo">
-            <p class="titulo buscador"> ${i.name} </p>
-            <p class="dinero"> ${i.currency} ${i.cost} </p>
-            <p class ="descripcion buscador"> ${i.description} </p>
-            <p class ="sold"> Unidades vendidas: ${i.soldCount}</p>
-            <button class="botonDeAgregar">Agregar a Carrito</button>
-          </div>
-        <div>
+  for (const i of registro.products){
+    producto.innerHTML +=
       `
-    }
-  };
+      <div class="articulo">
+      <div class="descDeArticulo">
+        <p class="titulo buscador"> ${i.name} </p>
+        <p class="dinero"> ${i.currency} ${i.cost} </p>
+        <p class ="sold"> Unidades vendidas: ${i.soldCount}</p>
+        <button class="botonDeAgregar">Agregar a Carrito</button>
+      </div>
+    <div>
+    `
+  }
+};
+
+function insertarNombre(name){
+  let header = document.getElementById("nombre");
+  if (name != ""){
+    header.innerHTML += name.toUpperCase();
+  } else {
+    header.innerHTML += "CONSUMIDOR FINAL"
+  }
+}
